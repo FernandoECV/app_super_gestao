ad720756d912dd3d440dfe250efefd2fe8260b4b
fernandoecv <fernandop.santossa@gmail.com>
2022-07-14 10:20:56 -0300

    Incluindo comentários e blocos PHP puros

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,15 @@
+<h3>Fornecedor</h3>
+
+{{'Texto de teste'}}
+
+{{-- Comentários --}}
+
+@php
+    // Para comentários de uma linha
+    /*
+        Para comentários de multiplas linhas 
+    */
+
+    echo '<br>Texto de teste';
+
+@endphp
